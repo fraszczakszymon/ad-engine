332085b8176479ebc0e027d0d7ffa46e81d143d7
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2016-09-23 13:36:37 +0200

    ADEN-3977 Fix logger and example page

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -2,15 +2,15 @@
 
 import * as QueryString from './query-string';
 
-const debugGroup = QueryString.get('adengine_debug'),
+const debugGroup = QueryString.get('adengine_debug') || '',
 	groups = debugGroup.split(',');
 
-if (!!debugGroup) {
+if (debugGroup !== '') {
 	console.info('AdEngine debug mode - groups:', debugGroup === '1' ? 'all' : groups);
 }
 
 export function logger(logGroup, ...logValues) {
-	if (!debugGroup) {
+	if (debugGroup === '') {
 		return;
 	}
 
