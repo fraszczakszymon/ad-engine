6ed6bd04f4bca73ca3af5507916fa2c302c75ec4
Wojtek Bulaszenko <wojciech.bulaszenko@wikia-inc.com>
2017-06-30 17:08:48 +0200

    ADEN-5432 added config

--- tests/testAdsTestAdsDfpParamsPresentFandom.js
+++ tests/testAdsTestAdsDfpParamsPresentFandom.js
@@ -1,15 +1,14 @@
-/**
- * Created by wojtek on 29/06/2017.
- */
-
 phantom.casperTest = true;
 
-var casper = require('casper').create();
+var config = require('config.json');
+var casper = require("casper").create();
 
 casper.test.begin('first test', 1, function(test) {
-    casper.start('http://localhost:5555/examples/templates/floating-ad/', function() {
+    casper.start(config.url.floatingAd, function() {
 
-        var expectedlLineItemId = '271491732';
+        var expectedlLineItemId = /\d{9}/;
+        var expectedCreativeId = /\d{12}/;
+        var expectedDataSize = '{"728x0":[[728,90]]}';
         var expectedCreativeSize = '[300,250]';
         var expectedPageLevelTargeting = '{"s1":"_project43"}';
         var expectedSlotTargeting = '{"loc":"top","pos":"TOP_LEADERBOARD","src":"gpt"}';
@@ -20,8 +19,14 @@ casper.test.begin('first test', 1, function(test) {
             var actualLineItemId = this.evaluate(function() {
                 return __utils__.findOne('#gpt-top-leaderboard').getAttribute('data-gpt-line-item-id');
             });
+            var actualCretiveId = this.evaluate(function() {
+                return __utils__.findOne('#gpt-top-leaderboard').getAttribute('data-gpt-creative-id');
+            });
             var actualCreativeSize = this.evaluate(function() {
                 return __utils__.findOne('#gpt-top-leaderboard').getAttribute('data-gpt-creative-size');
+            });
+            var actualDataSize = this.evaluate(function() {
+                return __utils__.findOne('#gpt-top-leaderboard').getAttribute('data-sizes');
             });
             var actualPageLevelTargeting = this.evaluate(function() {
                 return __utils__.findOne('#gpt-top-leaderboard').getAttribute('data-gpt-page-params');
@@ -36,8 +41,10 @@ casper.test.begin('first test', 1, function(test) {
             test.assertEquals(actualPageLevelTargeting, expectedPageLevelTargeting, actualPageLevelTargeting  + ' equals ' + expectedPageLevelTargeting);
             test.assertEquals(actualSlotTargeting, expectedSlotTargeting,  actualSlotTargeting + ' equals ' + expectedSlotTargeting);
             test.assertEquals(actualCreativeSize, expectedCreativeSize, actualCreativeSize + ' equals ' + expectedCreativeSize);
-            test.assertEquals(actualLineItemId, expectedlLineItemId, actualLineItemId +  ' equals ' + expectedlLineItemId);
+            test.assertEquals(actualDataSize, expectedDataSize, actualDataSize + ' equals ' + expectedDataSize);
             test.assertEquals(actualSlotResults, expectedSlotResults, actualSlotResults + ' equals ' + expectedSlotResults);
+            test.assertTruthy(expectedlLineItemId.test(actualLineItemId), 'LineItemId contains 9 numbers');
+            test.assertTruthy(expectedCreativeId.test(actualCretiveId), 'CreativeId contains 12 numbers');
         });
     }).run();
 
