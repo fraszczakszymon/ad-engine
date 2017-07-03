5a009fd9600b01af94dda4f0fc4e3bef2e320137
dianafa <diana.falkowska@gmail.com>
2017-07-03 10:53:52 +0200

    ADEN-5432
    refactor: move params to one array and iterate over it

--- tests/testAdsTestAdsDfpParamsPresentFandom.js
+++ tests/testAdsTestAdsDfpParamsPresentFandom.js
@@ -1,39 +1,56 @@
 phantom.casperTest = true;
 
 var config = require('config.json');
-var casper = require("casper").create();
+var casper = require('casper').create();
 var utils = require('utils');
 
 casper.test.begin('first test', 1, function(test) {
     casper.start(config.url.floatingAd, function() {
 
-        var expectedlLineItemId = /\d{9}/,
-            expectedCreativeId = /\d{12}/,
-            expectedDataSize = '{"728x0":[[728,90]]}',
-            expectedCreativeSize = '[300,250]',
-            expectedPageLevelTargeting = '{"s1":"_project43"}',
-            expectedSlotTargeting = '{"loc":"top","pos":"TOP_LEADERBOARD","src":"gpt"}',
-            expectedSlotResults = 'success';
+        var testCases = [
+            {
+                attr: 'data-gpt-creative-id',
+                expectedPattern: /\d{12}/
+            },
+            {
+                attr: 'data-gpt-line-item-id',
+                expectedPattern: /\d{9}/
+            },
+            {
+                attr: 'data-gpt-creative-size',
+                expectedString: '[300,250]'
+            },
+            {
+                attr: 'data-sizes',
+                expectedString: '{"728x0":[[728,90]]}'
+            },
+            {
+                attr: 'data-gpt-page-params',
+                expectedString: '{"s1":"_project43"}'
+            },
+            {
+                attr: 'data-gpt-slot-params',
+                expectedString: '{"loc":"top","pos":"TOP_LEADERBOARD","src":"gpt"}'
+            },
+            {
+                attr: 'data-slot-result',
+                expectedString: 'success'
+            }
+        ];
 
         casper.waitForResource('https://tpc.googlesyndication.com/pagead/imgad?id=CICAgKDL0suR1gEQARgBMggtHRiVJ9zWuA', function() {
-            var topLeaderboard = this.getElementInfo('div[id="gpt-top-leaderboard"]'),
-                actualCreativeId = topLeaderboard.attributes['data-gpt-creative-id'],
-                actualCreativeSize = topLeaderboard.attributes['data-gpt-creative-size'],
-                actualDataSize = topLeaderboard.attributes['data-sizes'],
-                actualLineItemId = topLeaderboard.attributes['data-gpt-line-item-id'],
-                actualPageLevelTargeting = topLeaderboard.attributes['data-gpt-page-params'],
-                actualSlotTargeting = topLeaderboard.attributes['data-gpt-slot-params'],
-                actualSlotResults = topLeaderboard.attributes['data-slot-result'];
+            var topLeaderboard = this.getElementInfo('div[id="gpt-top-leaderboard"]');
 
-            test.assertEquals(actualPageLevelTargeting, expectedPageLevelTargeting, actualPageLevelTargeting  + ' equals ' + expectedPageLevelTargeting);
-            test.assertEquals(actualSlotTargeting, expectedSlotTargeting,  actualSlotTargeting + ' equals ' + expectedSlotTargeting);
-            test.assertEquals(actualCreativeSize, expectedCreativeSize, actualCreativeSize + ' equals ' + expectedCreativeSize);
-            test.assertEquals(actualDataSize, expectedDataSize, actualDataSize + ' equals ' + expectedDataSize);
-            test.assertEquals(actualSlotResults, expectedSlotResults, actualSlotResults + ' equals ' + expectedSlotResults);
-            test.assertTruthy(expectedCreativeId.test(actualCreativeId), 'CreativeId contains 12 numbers');
-            test.assertTruthy(expectedlLineItemId.test(actualLineItemId), 'LineItemId contains 9 numbers');
+            testCases.forEach(function(testCase) {
+                actual = topLeaderboard.attributes[testCase.attr];
+
+                if (testCase.expectedPattern) {
+                    test.assertTruthy(testCase.expectedPattern.test(actual), testCase.attr + ' correctly fits in pattern');
+                } else {
+                    test.assertEquals(actual, testCase.expectedString, testCase.attr + ' equals ' + testCase.expectedString);
+                }
+            });
 
         });
     }).run();
-
 });
