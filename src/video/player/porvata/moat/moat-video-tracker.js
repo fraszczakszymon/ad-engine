f5ea71afb9342e5ffa185315039df19433e6bd5c
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2017-11-09 13:12:48 +0100

    ADEN-6155 Move MOAT partnerCode to context

--- src/video/player/porvata/moat/moat-video-tracker.js
+++ src/video/player/porvata/moat/moat-video-tracker.js
@@ -1,13 +1,13 @@
 import { logger } from '../../../../utils/logger';
+import Context from '../../../../services/context-service';
 import initMoatTracking from './moat-video-tracker-script';
 
-const logGroup = 'moat-video-tracker',
-	partnerCode = 'wikiaimajsint377461931603';
+const logGroup = 'moat-video-tracker';
 
 export default class MoatVideoTracker {
 	static init(adsManager, container, viewMode, slicer1, slicer2) {
 		const ids = {
-			partnerCode,
+			partnerCode: Context.get('options.video.moatTracking.partnerCode'),
 			viewMode,
 			slicer1,
 			slicer2
