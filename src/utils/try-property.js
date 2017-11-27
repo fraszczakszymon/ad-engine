d3a2ba34856fb81f793c6bc805b40c8bcf005a07
Maciej Leśniewski <mlesniewski@wikia-inc.com>
2017-11-27 17:34:00 +0100

    ADEN-6132 Adds tryProperty util, refactors native fullscreen

--- src/utils/try-property.js
+++ src/utils/try-property.js
@@ -1,16 +1,14 @@
 export default function tryProperty(obj, properties = []) {
-	let propertyValue;
-
 	for (const property of properties) {
 		if (typeof property !== 'string') {
 			throw new Error('property name must be a string');
 		}
 
 		if (property in obj) {
-			propertyValue = obj[property];
-			break;
+			const propertyValue = obj[property];
+			return (typeof propertyValue === 'function') ? propertyValue.bind(obj) : propertyValue;
 		}
 	}
 
-	return (typeof propertyValue === 'function') ? propertyValue.bind(obj) : propertyValue;
+	return null;
 }
