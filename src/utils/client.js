34cd9af6dbf11ff66f66e0a8e49aa503fa26bd0e
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2017-11-27 10:44:04 +0100

    ADEN-6259 Add OS and browser detect

--- src/utils/client.js
+++ src/utils/client.js
@@ -1,10 +1,16 @@
 import MobileDetect from 'mobile-detect';
 
-let mobileDetect = null;
+const appName = window.navigator.appName,
+	appVersion = window.navigator.appVersion,
+	userAgent = window.navigator.userAgent;
+
+let browser = null,
+	mobileDetect = null,
+	operatingSystem = null;
 
 function getMobileDetect() {
 	if (mobileDetect === null) {
-		mobileDetect = new MobileDetect(window.navigator.userAgent);
+		mobileDetect = new MobileDetect(userAgent);
 	}
 
 	return mobileDetect;
@@ -35,5 +41,61 @@ export default class Client {
 		}
 
 		return 'desktop';
+	}
+
+	static getOperatingSystem() {
+		if (operatingSystem !== null) {
+			return operatingSystem;
+		}
+
+		operatingSystem = 'unknown';
+		if (userAgent.indexOf('Win') !== -1) {
+			operatingSystem = 'Windows';
+		}
+		if (userAgent.indexOf('Mac') !== -1) {
+			operatingSystem = 'OSX';
+		}
+		if (userAgent.indexOf('Linux') !== -1) {
+			operatingSystem = 'Linux';
+		}
+		if (userAgent.indexOf('Android') !== -1) {
+			operatingSystem = 'Android';
+		}
+		if (userAgent.indexOf('like Mac') !== -1) {
+			operatingSystem = 'iOS';
+		}
+
+		return operatingSystem;
+	}
+
+	static getBrowser() {
+		if (browser !== null) {
+			return browser;
+		}
+
+		let temp,
+			matches = userAgent.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+
+		if (/trident/i.test(matches[1])) {
+			temp = /\brv[ :]+(\d+)/g.exec(userAgent) || [];
+			browser = `IE ${(temp[1] || '')}`;
+			return browser;
+		}
+		if (matches[1] === 'Chrome') {
+			temp= userAgent.match(/\b(OPR|Edge)\/(\d+)/);
+			if (temp !== null) {
+				browser = temp.slice(1).join(' ').replace('OPR', 'Opera');
+				return browser;
+			}
+		}
+
+		matches = matches[2] ? [matches[1], matches[2]] : [appName, appVersion, '-?'];
+		temp = userAgent.match(/version\/(\d+)/i);
+		if (temp !== null) {
+			matches.splice(1, 1, temp[1]);
+		}
+		browser = matches.join(' ');
+
+		return browser;
 	}
 }
