bdec015ae4c5355f497716398bfca2c028503b3e
Damian Sobczak <dsobczak@wikia-inc.com>
2018-01-18 10:29:40 +0100

    ADEN-6146 Fix ESLint warning

--- examples/utils/adblock-detect/script.js
+++ examples/utils/adblock-detect/script.js
@@ -3,6 +3,6 @@ import { utils } from '@wikia/ad-engine';
 const preElement = document.getElementById('checked');
 
 utils.client.checkBlocking(
-	() => { preElement.innerText = 'AdBlock enabled'; }, 
+	() => { preElement.innerText = 'AdBlock enabled'; },
 	() => { preElement.innerText = 'AdBlock disabled'; }
 );
