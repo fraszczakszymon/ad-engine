06998e614bdf8e2a1a72d9e46879c87741167c71
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2018-04-04 09:32:16 +0200

    ADEN-6957 Add unit tests

--- spec/ad-slot-fake.js
+++ spec/ad-slot-fake.js
@@ -1,3 +1,5 @@
+let offsetTop = 1000;
+
 export default {
 	getId() {
 		return 'gpt-fake-ad';
@@ -5,6 +7,28 @@ export default {
 
 	getSlotName() {
 		return 'FAKE_AD';
+	},
+
+	getViewportConflicts() {
+		return [
+			'foo-container'
+		];
+	},
+
+	getElement() {
+		return {
+			classList: {
+				contains: () => {}
+			},
+			offsetHeight: 300,
+			offsetTop,
+			offsetParent: null,
+			ownerDocument: {}
+		}
+	},
+
+	setOffsetTop(offset) {
+		offsetTop = offset;
 	},
 
 	config: {
--- spec/services/slot-service.spec.js
+++ spec/services/slot-service.spec.js
@@ -1,8 +1,25 @@
 import { expect } from 'chai';
+import sinon from 'sinon';
 import adSlotFake from '../ad-slot-fake';
 import { slotService } from '../../src/services/slot-service';
 
 describe('slot-service', () => {
+	afterEach(() => {
+		document.getElementById.restore();
+	});
+
+	beforeEach(() => {
+		sinon.stub(document, 'getElementById').withArgs('foo-container').returns({
+			classList: {
+				contains: () => {}
+			},
+			offsetHeight: 300,
+			offsetTop: 100,
+			offsetParent: null,
+			ownerDocument: {}
+		});
+	});
+
 	it('getter by id', () => {
 		slotService.add(adSlotFake);
 
@@ -21,5 +38,17 @@ describe('slot-service', () => {
 		slotService.forEach((adSlot) => {
 			expect(adSlotFake).to.equal(adSlot);
 		});
+	});
+
+	it('checks whether slot has viewport conflicts', () => {
+		adSlotFake.setOffsetTop(500);
+
+		expect(slotService.hasViewportConflict(adSlotFake)).to.equals(true);
+	});
+
+	it('checks whether slot does not have viewport conflicts (when there is enough space)', () => {
+		adSlotFake.setOffsetTop(2000);
+
+		expect(slotService.hasViewportConflict(adSlotFake)).to.equals(false);
 	});
 });
