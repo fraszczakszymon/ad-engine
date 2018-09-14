53a572b00e2661f73a6b667ec843b5bf8ebb851a
wojciechsygier <wsygier@wikia-inc.com>
2018-09-14 13:16:26 +0200

    Added Block BTF tests Added a page for Block btf tests

---
+++ tests/pages/block-btf-ad.page.js
@@ -0,0 +1,11 @@
+class BlockBtfAd {
+	constructor() {
+		this.pageLink = 'slots/block-btf/';
+		this.incontentBoxad = '#incontent_boxad';
+		this.topLeaderboard = '#top_leaderboard';
+		this.topLeaderboardWidth = 728;
+		this.topLeaderboardHeight = 90;
+	}
+}
+
+export default new BlockBtfAd();
---
+++ tests/specs/block-btf-ad.test.js
@@ -0,0 +1,48 @@
+import blockBtfAd from '../pages/block-btf-ad.page';
+import { timeouts } from '../common/timeouts';
+import helpers from '../common/helpers';
+
+const { expect } = require('chai');
+
+describe('It will test viewport conflicts ad page', () => {
+	beforeEach(() => {
+		browser.url(blockBtfAd.pageLink, timeouts.standard);
+		browser.waitForVisible(blockBtfAd.topLeaderboard, timeouts.standard);
+	});
+
+	it('will test top leaderboard visibility and dimensions', () => {
+		const size = browser.getElementSize(blockBtfAd.topLeaderboard);
+
+		expect(size.width)
+			.to
+			.equal(blockBtfAd.topLeaderboardWidth);
+		expect(size.height)
+			.to
+			.equal(blockBtfAd.topLeaderboardHeight);
+		expect(browser.isVisibleWithinViewport(blockBtfAd.topLeaderboard))
+			.to
+			.be
+			.true;
+	});
+
+	it(' will test top leaderboard ad redirect on click', () => {
+		browser.click(blockBtfAd.topLeaderboard);
+
+		const tabIds = browser.getTabIds();
+
+		browser.switchTab(tabIds[1]);
+		helpers.waitForUrl(helpers.newsAndStories);
+		expect(browser.getUrl())
+			.to
+			.equal(helpers.newsAndStories);
+		helpers.closeNewTabs();
+	});
+
+	it('will test if incontent boxad is hidden on the page', () => {
+		helpers.slowScroll(2000);
+		expect(browser.isVisibleWithinViewport(blockBtfAd.incontentBoxad))
+			.to
+			.be
+			.false;
+	});
+});
