35eac21d613c1e511a5a7b977f2d517bd1b6e255
wojciechsygier <wsygier@wikia-inc.com>
2018-09-28 13:06:01 +0200

    Rewrote every describe() to be independent from the others

--- tests/specs/hivi-uap-static-ad.test.js
+++ tests/specs/hivi-uap-static-ad.test.js
@@ -5,328 +5,354 @@ import helpers from '../common/helpers';
 
 const { expect } = require('chai');
 
-describe('It will test uap hivi static slots', () => {
+describe('It will test top leaderboard default and resolved state after refresh on hivi uap static', () => {
 	beforeEach(() => {
 		browser.url(hiviUapStatic.pageLink);
-		browser.waitForVisible(helpers.pageBody);
+		browser.waitForVisible(adSlots.topLeaderboard, timeouts.standard);
 	});
 
 	afterEach(() => {
 		browser.scroll(0, 0);
 	});
 
-	describe('It will test top leaderboard default and resolved state after refresh', () => {
-		it('will test default and resolved state after refresh', () => {
-			const defaultSize = browser.getElementSize(adSlots.topLeaderboard);
-			const tableOfErrors = [];
+	it('will test default and resolved state after refresh', () => {
+		const defaultSize = browser.getElementSize(adSlots.topLeaderboard);
+		const tableOfErrors = [];
 
-			try {
-				expect(defaultSize.width)
-					.to
-					.equal(adSlots.adProductsTopLeaderboardWidth, 'Default width incorrect');
-				expect(defaultSize.height)
-					.to
-					.equal(adSlots.uapTopLeaderboardHeight, 'Default height incorrect');
-			} catch (error) {
-				tableOfErrors.push(error.message);
-			}
-			try {
-				expect(browser.isVisibleWithinViewport(adSlots.topLeaderboard), 'Top leaderboard not in viewport')
-					.to
-					.be
-					.true;
-			} catch (error) {
-				tableOfErrors.push(error.message);
-			}
-
-			browser.refresh();
-			browser.waitForVisible(adSlots.topLeaderboard, timeouts.standard);
-
-			const resolvedSize = browser.getElementSize(adSlots.topLeaderboard);
-
-			try {
-				expect(resolvedSize.width)
-					.to
-					.equal(adSlots.adProductsTopLeaderboardWidth, 'Resolved width incorrect');
-				expect(resolvedSize.height)
-					.to
-					.equal(adSlots.uapTopLeaderboardHeightResolved, 'Resolved height incorrect');
-			} catch (error) {
-				tableOfErrors.push(error.message);
-			}
-			try {
-				expect(browser.isVisibleWithinViewport(adSlots.topLeaderboard), 'Top leaderboard not in viewport')
-					.to
-					.be
-					.true;
-			} catch (error) {
-				tableOfErrors.push(error.message);
-			}
-
-			expect(tableOfErrors.length, helpers.errorFormatter(tableOfErrors))
+		try {
+			expect(defaultSize.width)
 				.to
-				.equal(0);
-		});
-	});
-
-	describe('It will test top leaderboard default and resolved state after scroll', () => {
-		it('will test default and resolved state after scroll', () => {
-			const defaultSize = browser.getElementSize(adSlots.topLeaderboard);
-			const tableOfErrors = [];
-
-			try {
-				expect(defaultSize.width)
-					.to
-					.equal(adSlots.adProductsTopLeaderboardWidth, 'Default width incorrect');
-				expect(defaultSize.height)
-					.to
-					.equal(adSlots.uapTopLeaderboardHeight, 'Default height incorrect');
-			} catch (error) {
-				tableOfErrors.push(error.message);
-			}
-			try {
-				expect(browser.isVisibleWithinViewport(adSlots.topLeaderboard), 'Top leaderboard not in viewport')
-					.to
-					.be
-					.true;
-			} catch (error) {
-				tableOfErrors.push(error.message);
-			}
-
-			helpers.slowScroll(300);
-
-			const resolvedSize = browser.getElementSize(adSlots.topLeaderboard, timeouts.standard);
-
-			try {
-				expect(resolvedSize.width)
-					.to
-					.equal(adSlots.adProductsTopLeaderboardWidth, 'Resolved width incorrect');
-				expect(resolvedSize.height)
-					.to
-					.equal(adSlots.uapTopLeaderboardHeightResolved, 'Resolved height incorrect');
-			} catch (error) {
-				tableOfErrors.push(error.message);
-			}
-			try {
-				expect(browser.isVisibleWithinViewport(adSlots.topLeaderboard), 'Top leaderboard not in viewport')
-					.to
-					.be
-					.true;
-			} catch (error) {
-				tableOfErrors.push(error.message);
-			}
-
-			expect(tableOfErrors.length, helpers.errorFormatter(tableOfErrors))
+				.equal(adSlots.adProductsTopLeaderboardWidth, 'Default width incorrect');
+			expect(defaultSize.height)
 				.to
-				.equal(0);
-		});
-	});
-
-	describe('It will test top leaderboard', () => {
-		beforeEach(() => {
-			browser.waitForVisible(adSlots.topLeaderboard, timeouts.standard);
-		});
-
-		it('will test line item id', () => {
-			expect(browser.element(adSlots.topLeaderboard).getAttribute(adSlots.lineItemParam))
-				.to
-				.equal(hiviUapStatic.topLineItemId, 'Line item ID mismatch');
-		});
-
-		it('will test if leaderboard does not obstruct the navbar', () => {
-			expect(browser.isVisibleWithinViewport(helpers.navbar), 'Navbar not visible')
+				.equal(adSlots.uapTopLeaderboardHeight, 'Default height incorrect');
+		} catch (error) {
+			tableOfErrors.push(error.message);
+		}
+		try {
+			expect(browser.isVisibleWithinViewport(adSlots.topLeaderboard), 'Top leaderboard not in viewport')
 				.to
 				.be
 				.true;
-		});
+		} catch (error) {
+			tableOfErrors.push(error.message);
+		}
 
-		it('will test redirect on click', () => {
-			browser.click(adSlots.topLeaderboard);
+		browser.refresh();
+		browser.waitForVisible(adSlots.topLeaderboard, timeouts.standard);
 
-			const tabIds = browser.getTabIds();
+		const resolvedSize = browser.getElementSize(adSlots.topLeaderboard);
 
-			browser.switchTab(tabIds[1]);
-			helpers.waitForUrl(helpers.fandomWord);
-			expect(browser.getUrl())
+		try {
+			expect(resolvedSize.width)
 				.to
-				.include(helpers.fandomWord);
-			helpers.closeNewTabs();
-		});
-
-		it('will test closing the top leaderboard', () => {
-			browser.click(`${adSlots.topLeaderboard} ${hiviUapStatic.closeLeaderboardButton}`);
-			expect(browser.element(adSlots.topLeaderboard).getAttribute(hiviUapStatic.slotResult))
+				.equal(adSlots.adProductsTopLeaderboardWidth, 'Resolved width incorrect');
+			expect(resolvedSize.height)
 				.to
-				.equal(hiviUapStatic.slotCollapsed, 'Slot has not collapsed');
-		});
-
-		it('will test top leaderboard unsticking after scroll', () => {
-			helpers.slowScroll(3000);
-			expect(browser.isVisibleWithinViewport(adSlots.topLeaderboard), 'Top leaderboard in viewport')
+				.equal(adSlots.uapTopLeaderboardHeightResolved, 'Resolved height incorrect');
+		} catch (error) {
+			tableOfErrors.push(error.message);
+		}
+		try {
+			expect(browser.isVisibleWithinViewport(adSlots.topLeaderboard), 'Top leaderboard not in viewport')
 				.to
 				.be
-				.false;
-		});
+				.true;
+		} catch (error) {
+			tableOfErrors.push(error.message);
+		}
+
+		expect(tableOfErrors.length, helpers.errorFormatter(tableOfErrors))
+			.to
+			.equal(0);
+	});
+});
+
+describe('It will test top leaderboard default and resolved state after scroll on hivi uap static', () => {
+	beforeEach(() => {
+		browser.url(hiviUapStatic.pageLink);
+		browser.waitForVisible(adSlots.topLeaderboard, timeouts.standard);
 	});
 
-	describe('It will test top boxad', () => {
-		beforeEach(() => {
-			browser.waitForVisible(adSlots.topBoxad, timeouts.standard);
-		});
-
-		it('will test dimensions and visibility', () => {
-			const size = browser.getElementSize(adSlots.topBoxad);
-			const tableOfErrors = [];
-
-			try {
-				expect(size.width)
-					.to
-					.equal(adSlots.boxadWidth, 'Width incorrect');
-				expect(size.height)
-					.to
-					.equal(adSlots.boxadHeight, 'Height incorrect');
-			} catch (error) {
-				tableOfErrors.push(error.message);
-			}
-			try {
-				expect(browser.isVisibleWithinViewport(adSlots.topBoxad), 'Top boxad not in viewport')
-					.to
-					.be
-					.true;
-			} catch (error) {
-				tableOfErrors.push(error.message);
-			}
-
-			expect(tableOfErrors.length, helpers.errorFormatter(tableOfErrors))
-				.to
-				.equal(0);
-		});
-
-		it('will test line item id', () => {
-			expect(browser.element(adSlots.topBoxad).getAttribute(adSlots.lineItemParam))
-				.to
-				.equal(hiviUapStatic.topLineItemId, 'Line item ID mismatch');
-		});
-
-		it('will test redirect on click', () => {
-			browser.click(adSlots.topBoxad);
-
-			const tabIds = browser.getTabIds();
-
-			browser.switchTab(tabIds[1]);
-			helpers.waitForUrl(helpers.fandomWord);
-			expect(browser.getUrl())
-				.to
-				.include(helpers.fandomWord);
-			helpers.closeNewTabs();
-		});
+	afterEach(() => {
+		browser.scroll(0, 0);
 	});
 
-	describe('It will test incontent boxad', () => {
-		beforeEach(() => {
-			browser.scroll(0, 1000);
-			browser.waitForVisible(adSlots.incontentBoxad, timeouts.standard);
-		});
+	it('will test default and resolved state after scroll', () => {
+		const defaultSize = browser.getElementSize(adSlots.topLeaderboard);
+		const tableOfErrors = [];
 
-		it('will test dimensions and visibility', () => {
-			const size = browser.getElementSize(adSlots.incontentBoxad);
-			const tableOfErrors = [];
-
-			try {
-				expect(size.width)
-					.to
-					.equal(adSlots.boxadWidth, 'Width incorrect');
-				expect(size.height)
-					.to
-					.equal(adSlots.boxadHeight, 'Height incorrect');
-			} catch (error) {
-				tableOfErrors.push(error.message);
-			}
-			try {
-				expect(browser.isVisibleWithinViewport(adSlots.incontentBoxad), 'Incontent boxad not in viewport')
-					.to
-					.be
-					.true;
-			} catch (error) {
-				tableOfErrors.push(error.message);
-			}
-
-			expect(tableOfErrors.length, helpers.errorFormatter(tableOfErrors))
+		try {
+			expect(defaultSize.width)
 				.to
-				.equal(0);
-		});
-
-		it('will test line item id', () => {
-			expect(browser.element(adSlots.incontentBoxad).getAttribute(adSlots.lineItemParam))
+				.equal(adSlots.adProductsTopLeaderboardWidth, 'Default width incorrect');
+			expect(defaultSize.height)
 				.to
-				.equal(hiviUapStatic.bottomLineItemId, 'Line item ID mismatch');
-		});
-
-		it('will test redirect on click', () => {
-			browser.click(adSlots.incontentBoxad);
-
-			const tabIds = browser.getTabIds();
-
-			browser.switchTab(tabIds[1]);
-			helpers.waitForUrl(helpers.fandomWord);
-			expect(browser.getUrl())
+				.equal(adSlots.uapTopLeaderboardHeight, 'Default height incorrect');
+		} catch (error) {
+			tableOfErrors.push(error.message);
+		}
+		try {
+			expect(browser.isVisibleWithinViewport(adSlots.topLeaderboard), 'Top leaderboard not in viewport')
 				.to
-				.include(helpers.fandomWord);
-			helpers.closeNewTabs();
-		});
+				.be
+				.true;
+		} catch (error) {
+			tableOfErrors.push(error.message);
+		}
+
+		helpers.slowScroll(300);
+
+		const resolvedSize = browser.getElementSize(adSlots.topLeaderboard, timeouts.standard);
+
+		try {
+			expect(resolvedSize.width)
+				.to
+				.equal(adSlots.adProductsTopLeaderboardWidth, 'Resolved width incorrect');
+			expect(resolvedSize.height)
+				.to
+				.equal(adSlots.uapTopLeaderboardHeightResolved, 'Resolved height incorrect');
+		} catch (error) {
+			tableOfErrors.push(error.message);
+		}
+		try {
+			expect(browser.isVisibleWithinViewport(adSlots.topLeaderboard), 'Top leaderboard not in viewport')
+				.to
+				.be
+				.true;
+		} catch (error) {
+			tableOfErrors.push(error.message);
+		}
+
+		expect(tableOfErrors.length, helpers.errorFormatter(tableOfErrors))
+			.to
+			.equal(0);
+	});
+});
+
+describe('It will test top leaderboard on hivi uap static', () => {
+	beforeEach(() => {
+		browser.url(hiviUapStatic.pageLink);
+		browser.waitForVisible(adSlots.topLeaderboard, timeouts.standard);
 	});
 
-	describe('It will test bottom leaderboard', () => {
-		beforeEach(() => {
-			helpers.slowScroll(7000);
-			browser.waitForVisible(adSlots.bottomLeaderboard, timeouts.standard);
-		});
+	afterEach(() => {
+		browser.scroll(0, 0);
+	});
 
-		it('will test dimensions and visibility', () => {
-			const size = browser.getElementSize(adSlots.bottomLeaderboard);
-			const tableOfErrors = [];
+	it('will test line item id', () => {
+		expect(browser.element(adSlots.topLeaderboard).getAttribute(adSlots.lineItemParam))
+			.to
+			.equal(hiviUapStatic.topLineItemId, 'Line item ID mismatch');
+	});
 
-			try {
-				expect(size.width)
-					.to
-					.equal(adSlots.uapBottomLeaderboardWidth, 'Width incorrect');
-				expect(size.height)
-					.to
-					.equal(adSlots.uapBottomLeaderboardHeight, 'Height incorrect');
-			} catch (error) {
-				tableOfErrors.push(error.message);
-			}
-			try {
-				expect(browser.isVisibleWithinViewport(adSlots.bottomLeaderboard), 'Bottom leaderboard not in viewport')
-					.to
-					.be
-					.true;
-			} catch (error) {
-				tableOfErrors.push(error.message);
-			}
+	it('will test if leaderboard does not obstruct the navbar', () => {
+		expect(browser.isVisibleWithinViewport(helpers.navbar), 'Navbar not visible')
+			.to
+			.be
+			.true;
+	});
 
-			expect(tableOfErrors.length, helpers.errorFormatter(tableOfErrors))
+	it('will test redirect on click', () => {
+		browser.click(adSlots.topLeaderboard);
+
+		const tabIds = browser.getTabIds();
+
+		browser.switchTab(tabIds[1]);
+		helpers.waitForUrl(helpers.fandomWord);
+		expect(browser.getUrl())
+			.to
+			.include(helpers.fandomWord);
+		helpers.closeNewTabs();
+	});
+
+	it('will test closing the top leaderboard', () => {
+		browser.click(`${adSlots.topLeaderboard} ${hiviUapStatic.closeLeaderboardButton}`);
+		expect(browser.element(adSlots.topLeaderboard).getAttribute(hiviUapStatic.slotResult))
+			.to
+			.equal(hiviUapStatic.slotCollapsed, 'Slot has not collapsed');
+	});
+
+	it('will test top leaderboard unsticking after scroll', () => {
+		helpers.slowScroll(3000);
+		expect(browser.isVisibleWithinViewport(adSlots.topLeaderboard), 'Top leaderboard in viewport')
+			.to
+			.be
+			.false;
+	});
+});
+
+describe('It will test top boxad on hivi uap static', () => {
+	beforeEach(() => {
+		browser.url(hiviUapStatic.pageLink);
+		browser.waitForVisible(adSlots.topBoxad, timeouts.standard);
+	});
+
+	afterEach(() => {
+		browser.scroll(0, 0);
+	});
+
+	it('will test dimensions and visibility', () => {
+		const size = browser.getElementSize(adSlots.topBoxad);
+		const tableOfErrors = [];
+
+		try {
+			expect(size.width)
 				.to
-				.equal(0);
-		});
-
-		it('will test line item id', () => {
-			expect(browser.element(adSlots.bottomLeaderboard).getAttribute(adSlots.lineItemParam))
+				.equal(adSlots.boxadWidth, 'Width incorrect');
+			expect(size.height)
 				.to
-				.equal(hiviUapStatic.bottomLineItemId, 'Line item ID mismatch');
-		});
-
-		it('will test redirect on click', () => {
-			browser.click(adSlots.bottomLeaderboard);
-
-			const tabIds = browser.getTabIds();
-
-			browser.switchTab(tabIds[1]);
-			helpers.waitForUrl(helpers.fandomWord);
-			expect(browser.getUrl())
+				.equal(adSlots.boxadHeight, 'Height incorrect');
+		} catch (error) {
+			tableOfErrors.push(error.message);
+		}
+		try {
+			expect(browser.isVisibleWithinViewport(adSlots.topBoxad), 'Top boxad not in viewport')
 				.to
-				.include(helpers.fandomWord);
-			helpers.closeNewTabs();
-		});
+				.be
+				.true;
+		} catch (error) {
+			tableOfErrors.push(error.message);
+		}
+
+		expect(tableOfErrors.length, helpers.errorFormatter(tableOfErrors))
+			.to
+			.equal(0);
+	});
+
+	it('will test line item id', () => {
+		expect(browser.element(adSlots.topBoxad).getAttribute(adSlots.lineItemParam))
+			.to
+			.equal(hiviUapStatic.topLineItemId, 'Line item ID mismatch');
+	});
+
+	it('will test redirect on click', () => {
+		browser.click(adSlots.topBoxad);
+
+		const tabIds = browser.getTabIds();
+
+		browser.switchTab(tabIds[1]);
+		helpers.waitForUrl(helpers.fandomWord);
+		expect(browser.getUrl())
+			.to
+			.include(helpers.fandomWord);
+		helpers.closeNewTabs();
+	});
+});
+
+describe('It will test incontent boxad on hivi uap static', () => {
+	beforeEach(() => {
+		browser.url(hiviUapStatic.pageLink);
+		browser.scroll(0, 1000);
+		browser.waitForVisible(adSlots.incontentBoxad, timeouts.standard);
+	});
+
+	afterEach(() => {
+		browser.scroll(0, 0);
+	});
+
+	it('will test dimensions and visibility', () => {
+		const size = browser.getElementSize(adSlots.incontentBoxad);
+		const tableOfErrors = [];
+
+		try {
+			expect(size.width)
+				.to
+				.equal(adSlots.boxadWidth, 'Width incorrect');
+			expect(size.height)
+				.to
+				.equal(adSlots.boxadHeight, 'Height incorrect');
+		} catch (error) {
+			tableOfErrors.push(error.message);
+		}
+		try {
+			expect(browser.isVisibleWithinViewport(adSlots.incontentBoxad), 'Incontent boxad not in viewport')
+				.to
+				.be
+				.true;
+		} catch (error) {
+			tableOfErrors.push(error.message);
+		}
+
+		expect(tableOfErrors.length, helpers.errorFormatter(tableOfErrors))
+			.to
+			.equal(0);
+	});
+
+	it('will test line item id', () => {
+		expect(browser.element(adSlots.incontentBoxad).getAttribute(adSlots.lineItemParam))
+			.to
+			.equal(hiviUapStatic.bottomLineItemId, 'Line item ID mismatch');
+	});
+
+	it('will test redirect on click', () => {
+		browser.click(adSlots.incontentBoxad);
+
+		const tabIds = browser.getTabIds();
+
+		browser.switchTab(tabIds[1]);
+		helpers.waitForUrl(helpers.fandomWord);
+		expect(browser.getUrl())
+			.to
+			.include(helpers.fandomWord);
+		helpers.closeNewTabs();
+	});
+});
+
+describe('It will test bottom leaderboard on hivi uap static', () => {
+	beforeEach(() => {
+		helpers.slowScroll(7000);
+		browser.waitForVisible(adSlots.bottomLeaderboard, timeouts.standard);
+	});
+
+	afterEach(() => {
+		browser.scroll(0, 0);
+	});
+
+	it('will test dimensions and visibility', () => {
+		const size = browser.getElementSize(adSlots.bottomLeaderboard);
+		const tableOfErrors = [];
+
+		try {
+			expect(size.width)
+				.to
+				.equal(adSlots.uapBottomLeaderboardWidth, 'Width incorrect');
+			expect(size.height)
+				.to
+				.equal(adSlots.uapBottomLeaderboardHeight, 'Height incorrect');
+		} catch (error) {
+			tableOfErrors.push(error.message);
+		}
+		try {
+			expect(browser.isVisibleWithinViewport(adSlots.bottomLeaderboard), 'Bottom leaderboard not in viewport')
+				.to
+				.be
+				.true;
+		} catch (error) {
+			tableOfErrors.push(error.message);
+		}
+
+		expect(tableOfErrors.length, helpers.errorFormatter(tableOfErrors))
+			.to
+			.equal(0);
+	});
+
+	it('will test line item id', () => {
+		expect(browser.element(adSlots.bottomLeaderboard).getAttribute(adSlots.lineItemParam))
+			.to
+			.equal(hiviUapStatic.bottomLineItemId, 'Line item ID mismatch');
+	});
+
+	it('will test redirect on click', () => {
+		browser.click(adSlots.bottomLeaderboard);
+
+		const tabIds = browser.getTabIds();
+
+		browser.switchTab(tabIds[1]);
+		helpers.waitForUrl(helpers.fandomWord);
+		expect(browser.getUrl())
+			.to
+			.include(helpers.fandomWord);
+		helpers.closeNewTabs();
 	});
 });
