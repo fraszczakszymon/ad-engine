1beda27236c0b93cd7eb029e93f1d3fe498dd272
Damian Sobczak <dsobczak@wikia-inc.com>
2018-10-04 09:45:02 +0200

    ADEN-7683 Adding sticky slot animations and close button

--- src/ad-products/templates/sticky-ad.js
+++ src/ad-products/templates/sticky-ad.js
@@ -9,6 +9,7 @@ import {
 	SLIDE_OUT_TIME
 } from './uap/constants';
 import { animate } from './interface/animate';
+import CloseButton from './interface/close-button';
 
 export class StickyAd {
 	static DEFAULT_UNSTICK_DELAY = 3000;
@@ -20,7 +21,6 @@ export class StickyAd {
 	static getDefaultConfig() {
 		return {
 			enabled: true,
-			stickinessAllowed: true,
 			stickyAdditionalTime: 0,
 			stickyUntilSlotViewed: true,
 			handleNavbar: true,
@@ -37,10 +37,7 @@ export class StickyAd {
 	}
 
 	static isEnabled() {
-		const templateName = StickyAd.getName();
-
-		return context.get(`templates.${templateName}.enabled`)
-			&& context.get(`templates.${templateName}.stickinessAllowed`);
+		return context.get(`templates.${StickyAd.getName()}.enabled`);
 	}
 
 	init(params) {
@@ -53,7 +50,6 @@ export class StickyAd {
 		this.adSlot.getElement().classList.add(CSS_CLASSNAME_STICKY_TEMPLATE);
 
 		this.addUnstickLogic();
-		this.addUnstickButton();
 		this.addUnstickEvents();
 
 		if (this.config.handleNavbar) {
@@ -89,13 +85,24 @@ export class StickyAd {
 	}
 
 	addUnstickButton() {
-		// nope
+		this.closeButton = new CloseButton({
+			classNames: ['button-unstick'],
+			onClick: () => {
+				(this.stickiness || this.stickiness).close();
+			}
+		}).render();
+
+		this.adSlot.getElement().firstChild.appendChild(this.closeButton);
+	}
+
+	removeUnstickButton() {
+		this.closeButton.remove();
 	}
 
 	addUnstickEvents() {
 		this.stickiness.on(Stickiness.STICKINESS_CHANGE_EVENT, isSticky => this.onStickinessChange(isSticky));
-		//this.stickiness.on(Stickiness.CLOSE_CLICKED_EVENT, this.onCloseClicked.bind(this));
-		//this.stickiness.on(Stickiness.UNSTICK_IMMEDIATELY_EVENT, this.unstickImmediately.bind(this));
+		this.stickiness.on(Stickiness.CLOSE_CLICKED_EVENT, this.unstickImmediately.bind(this));
+		this.stickiness.on(Stickiness.UNSTICK_IMMEDIATELY_EVENT, this.unstickImmediately.bind(this));
 	}
 
 	async onStickinessChange(isSticky) {
@@ -104,9 +111,22 @@ export class StickyAd {
 			this.adSlot.getElement().classList.remove(CSS_CLASSNAME_STICKY_SLOT);
 			this.adSlot.getElement().firstChild.style.top = 0;
 			animate(this.adSlot.getElement().firstChild, CSS_CLASSNAME_FADE_IN_ANIMATION, FADE_IN_TIME);
+
+			this.removeUnstickButton();
 		} else {
 			this.adSlot.getElement().classList.add(CSS_CLASSNAME_STICKY_SLOT);
 			this.adSlot.getElement().firstChild.style.top = `${this.config.topOffset}px`;
+
+			this.addUnstickButton();
+		}
+	}
+
+	unstickImmediately() {
+		if (this.stickiness) {
+			this.adSlot.getElement().classList.remove(CSS_CLASSNAME_STICKY_SLOT);
+			this.stickiness.sticky = false;
+
+			this.removeUnstickButton();
 		}
 	}
 }
