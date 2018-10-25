000cc1f5fa1e68b737b3f864314469713eae1087
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2018-10-25 09:38:11 +0200

    ADEN-7783 Fix description

--- tests/specs/interstitial.test.js
+++ tests/specs/interstitial.test.js
@@ -14,21 +14,21 @@ describe('Interstitial page: interstitial', () => {
 		browser.waitForVisible(interstitial.closeButton, timeouts.standard);
 		browser.waitForVisible(interstitial.advertisementLabel, timeouts.standard);
 
-		expect(browser.isVisibleWithinViewport(adSlots.invisibleHighImpact, 'Floor adhesion not in the viewport'))
+		expect(browser.isVisibleWithinViewport(adSlots.invisibleHighImpact, 'Interstitial not in the viewport'))
 			.to
 			.be
 			.true;
 	});
 
 	it('Check if interstitial disappears after clicking close button', () => {
-		expect(browser.isVisibleWithinViewport(adSlots.invisibleHighImpact, 'Floor adhesion not in the viewport'))
+		expect(browser.isVisibleWithinViewport(adSlots.invisibleHighImpact, 'Interstitial not in the viewport'))
 			.to
 			.be
 			.true;
 		browser.waitForExist(interstitial.stopScrolling, timeouts.standard);
 
 		browser.click(interstitial.closeButton);
-		expect(browser.isVisibleWithinViewport(adSlots.invisibleHighImpact, 'Floor adhesion is in the viewport'))
+		expect(browser.isVisibleWithinViewport(adSlots.invisibleHighImpact, 'Interstitial is in the viewport'))
 			.to
 			.be
 			.false;
