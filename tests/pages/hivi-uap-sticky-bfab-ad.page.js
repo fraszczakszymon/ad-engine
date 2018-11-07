8224996217e2ec1a8a9cc4cafe5feedc72a880ff
wojciechsygier <wsygier@wikia-inc.com>
2018-11-07 13:06:46 +0100

    Slightly modified video player selectors Removed no longer needed selector from sticky BFAB

--- tests/pages/hivi-uap-sticky-bfab-ad.page.js
+++ tests/pages/hivi-uap-sticky-bfab-ad.page.js
@@ -4,7 +4,6 @@ class HiviUapStickyBfab {
 		this.videoPlayer = '.video-player.video-player-right';
 		this.fullScreen = '.stop-scrolling';
 		this.slotResult = 'data-slot-result';
-		this.closeLeaderboardButton = 'button';
 		this.bottomLineItemId = '4511050296'; // bottom leaderboard and incontent boxad share the same ID
 		this.videoLength = 45000;
 	}
