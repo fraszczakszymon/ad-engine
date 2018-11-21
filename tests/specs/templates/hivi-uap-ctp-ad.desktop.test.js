4165df963676ff1fbe5765bce5ddb1514c453b28
emilmadry <emadry@wikia-inc.com>
2018-11-21 15:52:47 +0100

    ADEN-7955 Import fixed

--- tests/specs/templates/hivi-uap-ctp-ad.desktop.test.js
+++ tests/specs/templates/hivi-uap-ctp-ad.desktop.test.js
@@ -1,5 +1,5 @@
 import hiviUapCtp from '../../pages/hivi-uap-ctp-ad.page';
-import adSlots from '../../common/adSlots';
+import adSlots from '../../common/ad-slots';
 import { timeouts } from '../../common/timeouts';
 import helpers from '../../common/helpers';
 
