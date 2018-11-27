401348560881a331fa2cbbe64ce5ce26c7aedfb1
Damian Sobczak <dsobczak@wikia-inc.com>
2018-11-27 16:25:58 +0100

    ADEN-7990 Add geo detection to stickyAd template

--- examples/templates/sticky-ad/script.js
+++ examples/templates/sticky-ad/script.js
@@ -3,7 +3,7 @@ import { StickyAd } from '@wikia/ad-products';
 import adContext from '../../context';
 
 context.extend(adContext);
-context.set('templates.stickyAd.lineItemIds', ['271491732']);
+context.set('templates.stickyAd.lineItemIds', ['271491732:XX']);
 context.set('slots.top_leaderboard.defaultTemplates', ['stickyAd']);
 
 // Include anything from ad-products to register StickyAd template or do it manually
