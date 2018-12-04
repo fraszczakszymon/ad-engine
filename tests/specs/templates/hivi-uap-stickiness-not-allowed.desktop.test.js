3d16d3bed261cf1f600783e2e4e08f7c91ff472e
emilmadry <emadry@wikia-inc.com>
2018-12-04 22:03:32 +0100

    ADEN-8010 No stickiness template tests

--- tests/specs/templates/hivi-uap-stickiness-not-allowed.desktop.test.js
+++ tests/specs/templates/hivi-uap-stickiness-not-allowed.desktop.test.js
@@ -1,12 +1,12 @@
 import { expect } from 'chai';
-import { hiviUapStickyBfab } from '../../pages/hivi-uap-sticky-bfab-ad.page';
+import { hiviUapStickinessNotAllowed } from '../../pages/hivi-uap-stickiness-not-allowed.page';
 import { adSlots } from '../../common/ad-slots';
 import { timeouts } from '../../common/timeouts';
 import { helpers } from '../../common/helpers';
 
-describe('Desktop HiVi UAP sticky BFAB ads page: top leaderboard', () => {
+describe('Desktop HiVi UAP sticky ads page: top leaderboard', () => {
 	beforeEach(() => {
-		browser.url(hiviUapStickyBfab.pageLink);
+		browser.url(hiviUapStickinessNotAllowed.pageLink);
 		browser.waitForVisible(adSlots.topLeaderboard, timeouts.standard);
 	});
 
@@ -14,13 +14,13 @@ describe('Desktop HiVi UAP sticky BFAB ads page: top leaderboard', () => {
 		helpers.waitForLineItemIdAttribute(adSlots.topLeaderboard);
 		expect(helpers.getLineItemId(adSlots.topLeaderboard))
 			.to
-			.equal(hiviUapStickyBfab.firstCall, 'Line item ID mismatch');
+			.equal(hiviUapStickinessNotAllowed.firstCall, 'Line item ID mismatch');
 	});
 });
 
-describe('Desktop HiVi UAP sticky BFAB ads page: top boxad', () => {
+describe('Desktop HiVi UAP sticky ads page: top boxad', () => {
 	beforeEach(() => {
-		browser.url(hiviUapStickyBfab.pageLink);
+		browser.url(hiviUapStickinessNotAllowed.pageLink);
 		browser.waitForVisible(adSlots.topBoxad, timeouts.standard);
 	});
 
@@ -28,13 +28,13 @@ describe('Desktop HiVi UAP sticky BFAB ads page: top boxad', () => {
 		helpers.waitForLineItemIdAttribute(adSlots.topBoxad);
 		expect(helpers.getLineItemId(adSlots.topBoxad))
 			.to
-			.equal(hiviUapStickyBfab.secondCall, 'Line item ID mismatch');
+			.equal(hiviUapStickinessNotAllowed.secondCall, 'Line item ID mismatch');
 	});
 });
 
-describe('Desktop HiVi UAP sticky BFAB ads page: incontent boxad', () => {
+describe('Desktop HiVi UAP sticky ads page: incontent boxad', () => {
 	beforeEach(() => {
-		browser.url(hiviUapStickyBfab.pageLink);
+		browser.url(hiviUapStickinessNotAllowed.pageLink);
 		browser.waitForVisible(adSlots.topLeaderboard);
 		browser.scroll(0, 1000);
 		browser.waitForVisible(adSlots.incontentBoxad, timeouts.standard);
@@ -44,26 +44,17 @@ describe('Desktop HiVi UAP sticky BFAB ads page: incontent boxad', () => {
 		helpers.waitForLineItemIdAttribute(adSlots.incontentBoxad);
 		expect(helpers.getLineItemId(adSlots.incontentBoxad))
 			.to
-			.equal(hiviUapStickyBfab.secondCall, 'Line item ID mismatch');
+			.equal(hiviUapStickinessNotAllowed.secondCall, 'Line item ID mismatch');
 	});
 });
 
-describe('Desktop HiVi UAP sticky BFAB ads page: bottom leaderboard', () => {
+describe('Desktop HiVi UAP sticky ads page: bottom leaderboard', () => {
 	let adStatus;
-	let defaultDimensions;
 	let refreshDimensions;
 	let videoFinishedDimensions;
 
 	before(() => {
-		hiviUapStickyBfab.openUapWithState(false, hiviUapStickyBfab.pageLink, adSlots.topLeaderboard);
-		browser.scroll(0, 3000);
-		adSlots.waitForSlotExpanded(adSlots.bottomLeaderboard);
-
-		defaultDimensions = adSlots.checkDerivativeSizeSlotRatio(adSlots.bottomLeaderboard,
-			helpers.wrapper,
-			adSlots.defaultDesktopRatio);
-
-		hiviUapStickyBfab.openUapWithState(true, hiviUapStickyBfab.pageLink, adSlots.topLeaderboard);
+		hiviUapStickinessNotAllowed.openUapWithState(true, hiviUapStickinessNotAllowed.pageLink, adSlots.topLeaderboard);
 		browser.scroll(0, 3000);
 		browser.waitForExist(adSlots.bottomLeaderboard, timeouts.standard);
 		browser.scroll(adSlots.bottomLeaderboard);
@@ -71,12 +62,12 @@ describe('Desktop HiVi UAP sticky BFAB ads page: bottom leaderboard', () => {
 		refreshDimensions = adSlots.checkDerivativeSizeSlotRatio(adSlots.bottomLeaderboard,
 			helpers.wrapper,
 			adSlots.resolvedDesktopRatio);
-		browser.url(hiviUapStickyBfab.pageLink);
+		browser.url(hiviUapStickinessNotAllowed.pageLink);
 		browser.waitForVisible(adSlots.topLeaderboard, timeouts.standard);
 		browser.scroll(0, 3000);
 		browser.waitForExist(adSlots.bottomLeaderboard, timeouts.standard);
 		browser.scroll(adSlots.bottomLeaderboard);
-		helpers.waitForVideoAdToFinish(hiviUapStickyBfab.videoLength);
+		helpers.waitForVideoAdToFinish(hiviUapStickinessNotAllowed.videoLength);
 
 		videoFinishedDimensions = adSlots.checkUAPSizeSlotRatio(adSlots.topLeaderboard,
 			adSlots.resolvedDesktopRatio);
@@ -92,13 +83,6 @@ describe('Desktop HiVi UAP sticky BFAB ads page: bottom leaderboard', () => {
 
 	it('Check if slot is visible in viewport', () => {
 		expect(adStatus.inViewport, 'Not in viewport')
-			.to
-			.be
-			.true;
-	});
-
-	it('Check if default dimensions are correct', () => {
-		expect(defaultDimensions.status, defaultDimensions.capturedErrors)
 			.to
 			.be
 			.true;
@@ -122,7 +106,7 @@ describe('Desktop HiVi UAP sticky BFAB ads page: bottom leaderboard', () => {
 		helpers.waitForLineItemIdAttribute(adSlots.bottomLeaderboard);
 		expect(helpers.getLineItemId(adSlots.bottomLeaderboard))
 			.to
-			.equal(hiviUapStickyBfab.secondCall, 'Line item ID mismatch');
+			.equal(hiviUapStickinessNotAllowed.secondCall, 'Line item ID mismatch');
 	});
 
 	it('Check if redirect on click works properly', () => {
@@ -136,13 +120,13 @@ describe('Desktop HiVi UAP sticky BFAB ads page: bottom leaderboard', () => {
 	it('Check if slot is sticked', () => {
 		browser.refresh();
 		browser.waitForVisible(adSlots.topLeaderboard, timeouts.standard);
-		helpers.waitToStartPlaying(); // TODO will not stick if scrolled earlier - problems with viewabillity counted?
+		helpers.waitToStartPlaying();
 		helpers.slowScroll(2500);
 		browser.waitForVisible(adSlots.bottomLeaderboard, timeouts.standard);
 		expect(browser.isVisibleWithinViewport(adSlots.bottomLeaderboard))
 			.to
 			.be
-			.true;
+			.false;
 		helpers.slowScroll(500, adSlots.bottomLeaderboard);
 		expect(browser.isVisible(adSlots.bottomLeaderboard))
 			.to
