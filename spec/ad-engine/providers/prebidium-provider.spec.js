fdff8a0d0e6e4c80cb891d9ca33c7753602b9f05
Damian Bielecki <dbielecki@fandom.com>
2018-12-11 16:53:50 +0100

    ADEN-7984 Add PrebidiumProvider tests

--- spec/ad-engine/providers/prebidium-provider.spec.js
+++ spec/ad-engine/providers/prebidium-provider.spec.js
@@ -6,6 +6,15 @@ describe('PrebidiumProvider', () => {
 	let sandbox;
 	let prebidiumProvider;
 	const stub = {
+		// utils: {
+		// 	logger: undefined,
+		// },
+		slotService: {
+			add: undefined,
+		},
+		context: {
+			get: undefined,
+		},
 		que: {
 			push: undefined,
 		},
@@ -22,25 +31,56 @@ describe('PrebidiumProvider', () => {
 		prebidiumProvider = new PrebidiumProvider();
 
 		stubIframeBuilder();
-		sandbox.stub(slotService, 'add');
-		sandbox.stub(context, 'get').returns(mock.adId);
+		stub.slotService.add = sandbox.stub(slotService, 'add');
+		stub.context.get = sandbox.stub(context, 'get').returns(mock.adId);
+		// stub.utils.logger = sandbox.stub(utils, 'logger');
 		stubPbjs();
 	});
 
 	describe('fillIn', () => {
+		let adSlot;
+
+		beforeEach(() => {
+			adSlot = {
+				getSlotName: () => mock.slotName,
+			};
+			prebidiumProvider.fillIn(adSlot);
+		});
+
 		it('should call renderAd', () => {
-			prebidiumProvider.fillIn(getAdSlot());
 			const [doc, adId] = stub.renderAd.getCall(0).args;
 
-			assert(stub.renderAd.called);
+			assert(stub.renderAd.calledOnce);
 			assert.equal(doc, mock.doc);
 			assert.equal(adId, mock.adId);
 		});
 
 		it('should be postponed until pbjs loads', () => {
-			prebidiumProvider.fillIn(getAdSlot());
-			assert(stub.que.push.called, `Function did not wait for pbjs to load`);
+			assert(stub.que.push.calledOnce);
 		});
+
+		it('should call slotService add with correct argument', () => {
+			const argument = stub.slotService.add.getCall(0).args[0];
+
+			assert(stub.slotService.add.calledOnce);
+			assert.equal(argument, adSlot);
+		});
+
+		it('should call context get with correct argument', () => {
+			const argument = stub.context.get.getCall(0).args[0];
+
+			assert(stub.context.get.calledOnce);
+			assert.equal(argument, `slots.${mock.slotName}.targeting.hb_adid`);
+		});
+
+		// it('should call logger', () => {
+		// 	const [logGroup, slotName, message] = stub.utils.logger.getCall(0).args;
+		//
+		// 	assert(stub.utils.logger.calledOnce);
+		// 	assert.equal(logGroup, 'prebidium-provider');
+		// 	assert.equal(slotName, mock.slotName);
+		// 	assert.equal(message, 'slot added');
+		// });
 	});
 
 	afterEach(() => {
@@ -62,11 +102,5 @@ describe('PrebidiumProvider', () => {
 		};
 		stub.renderAd = sandbox.stub(window.pbjs, 'renderAd');
 		stub.que.push = sandbox.stub(window.pbjs.que, 'push').callsFake((method) => method());
-	}
-
-	function getAdSlot() {
-		return {
-			getSlotName: () => mock.slotName,
-		};
 	}
 });
