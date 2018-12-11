2464ae5c91f604e911394b00946a92458a420082
Damian Bielecki <dbielecki@fandom.com>
2018-12-11 15:40:00 +0100

    ADEN-7984 Refactor mocking

--- spec/ad-engine/providers/prebidium-provider.spec.js
+++ spec/ad-engine/providers/prebidium-provider.spec.js
@@ -5,41 +5,68 @@ import { context, PrebidiumProvider, slotService } from '@wikia/ad-engine';
 describe('PrebidiumProvider', () => {
 	let sandbox;
 	let prebidiumProvider;
-	let quePush;
-	let renderAd;
+	const stub = {
+		que: {
+			push: undefined,
+		},
+		renderAd: undefined,
+	};
+	const mock = {
+		doc: 'mock_document',
+		adId: 'mock_ad_id',
+		slotName: 'mock_slot_name',
+	};
 
 	beforeEach(() => {
 		sandbox = sinon.sandbox.create();
-
 		prebidiumProvider = new PrebidiumProvider();
-		sandbox.stub(prebidiumProvider.iframeBuilder, 'create').returns({
-			contentWindow: {
-				document: {},
-			},
-		});
 
+		stubIframeBuilder();
 		sandbox.stub(slotService, 'add');
-		sandbox.stub(context, 'get').returns('test_ad_id');
-
-		window.pbjs = {
-			renderAd: () => {},
-			que: [],
-		};
-		renderAd = sandbox.stub(window.pbjs, 'renderAd');
-		quePush = sandbox.stub(window.pbjs.que, 'push').callsFake((method) => method());
+		sandbox.stub(context, 'get').returns(mock.adId);
+		stubPbjs();
 	});
 
-	it('should call renderAd', () => {
-		const adSlot = {
-			getSlotName: () => 'test_slot_name',
-		};
+	describe('fillIn', () => {
+		it('should call renderAd', () => {
+			prebidiumProvider.fillIn(getAdSlot());
+			const [doc, adId] = stub.renderAd.getCall(0).args;
 
-		prebidiumProvider.fillIn(adSlot);
-		assert(quePush.called, `Function did not wait for pbjs to load`);
-		assert(renderAd.called, 'Function did not call render add');
+			assert(stub.renderAd.called);
+			assert.equal(doc, mock.doc);
+			assert.equal(adId, mock.adId);
+		});
+
+		it('should be postponed until pbjs loads', () => {
+			prebidiumProvider.fillIn(getAdSlot());
+			assert(stub.que.push.called, `Function did not wait for pbjs to load`);
+		});
 	});
 
 	afterEach(() => {
 		sandbox.restore();
 	});
+
+	function stubIframeBuilder() {
+		sandbox.stub(prebidiumProvider.iframeBuilder, 'create').returns({
+			contentWindow: {
+				document: mock.doc,
+			},
+		});
+	}
+
+	function stubPbjs() {
+		window.pbjs = {
+			renderAd: () => {},
+			que: [],
+		};
+		stub.renderAd = sandbox.stub(window.pbjs, 'renderAd');
+		stub.que.push = sandbox.stub(window.pbjs.que, 'push').callsFake((method) => method());
+	}
+
+	function getAdSlot() {
+		return {
+			getSlotName: () => mock.slotName,
+		};
+	}
 });
