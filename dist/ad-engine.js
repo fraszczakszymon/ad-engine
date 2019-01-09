e6edf5829a9afa586021369cfe0f287da1557e80
Jenkins <jenkins@fandom.com>
2019-01-09 10:21:49 +0000

    23.1.0

--- dist/ad-engine.js
+++ dist/ad-engine.js
@@ -5756,9 +5756,9 @@ if (get_default()(window, versionField, null)) {
 	window.console.warn('Multiple @wikia/ad-engine initializations. This may cause issues.');
 }
 
-set_default()(window, versionField, 'v23.0.2');
-set_default()(window, commitField, '1d8f032');
-logger('ad-engine', 'v23.0.2 (1d8f032)');
+set_default()(window, versionField, 'v23.1.0');
+set_default()(window, commitField, 'd13bb0c1');
+logger('ad-engine', 'v23.1.0 (d13bb0c1)');
 
 
 
