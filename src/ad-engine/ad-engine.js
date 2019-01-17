3f259e7e6ae7eae58b18bbcba9db134ef6149234
Damian Bielecki <dbielecki@fandom.com>
2019-01-17 17:02:34 +0100

    ADEN-8145 Use old LazyQueue for adStack

--- src/ad-engine/ad-engine.js
+++ src/ad-engine/ad-engine.js
@@ -1,4 +1,4 @@
-import { LazyQueue, logger } from './utils';
+import { LazyQueue, logger, makeLazyQueue } from './utils';
 import { FloatingAd } from './templates';
 import { GptProvider, PrebidiumProvider } from './providers';
 import { scrollListener } from './listeners';
@@ -29,13 +29,13 @@ export class AdEngine {
 
 		events.on(events.PAGE_CHANGE_EVENT, () => {
 			this.started = false;
-			this.setupAdStackQueue();
+			this.setupAdStack();
 		});
 	}
 
 	init() {
 		this.setupProviders();
-		this.setupAdStackQueue();
+		this.setupAdStack();
 		btfBlockerService.init();
 
 		registerCustomAdLoader(context.get('options.customAdLoader.globalMethodName'));
@@ -67,21 +67,16 @@ export class AdEngine {
 	/**
 	 * @private
 	 */
-	setupAdStackQueue() {
-		const adStack = context.get('state.adStack');
+	setupAdStack() {
+		this.adStack = context.get('state.adStack');
+		if (!this.adStack.start) {
+			makeLazyQueue(this.adStack, (ad) => {
+				const adSlot = new AdSlot(ad);
 
-		if (adStack instanceof LazyQueue) {
-			return;
+				slotService.add(adSlot);
+				this.provider.fillIn(adSlot);
+			});
 		}
-
-		this.adStackQueue = new LazyQueue(...adStack);
-		this.adStackQueue.onItemFlush((ad) => {
-			const adSlot = new AdSlot(ad);
-
-			slotService.add(adSlot);
-			this.provider.fillIn(adSlot);
-		});
-		context.set('state.adStack', this.adStackQueue);
 	}
 
 	/**
@@ -93,7 +88,7 @@ export class AdEngine {
 			const pushOnScrollQueue = new LazyQueue(...pushOnScrollIds);
 
 			pushOnScrollQueue.onItemFlush((id) => {
-				scrollListener.addSlot(this.adStackQueue, id, context.get('events.pushOnScroll.threshold'));
+				scrollListener.addSlot(this.adStack, id, context.get('events.pushOnScroll.threshold'));
 			});
 			context.set('events.pushOnScroll.ids', pushOnScrollQueue);
 			pushOnScrollQueue.flush();
@@ -110,7 +105,7 @@ export class AdEngine {
 		await Promise.race([Promise.all(delayModulesPromises), timeoutPromise]);
 
 		logger(logGroup, 'startAdQueue', 'Ready');
-		this.flushAdStackQueue();
+		this.startAdStack();
 	}
 
 	/**
@@ -132,11 +127,11 @@ export class AdEngine {
 	/**
 	 * @private
 	 */
-	flushAdStackQueue() {
+	startAdStack() {
 		if (!this.started) {
 			events.emit(events.AD_STACK_START);
 			this.started = true;
-			this.adStackQueue.flush();
+			this.adStack.start();
 		}
 	}
 }
