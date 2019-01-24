e1e316c38c46517d3b77ce18ee897ad0374d4667
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2019-01-24 10:47:37 +0100

    ADEN-8190 Rename suites

--- wdio.conf.js
+++ wdio.conf.js
@@ -15,10 +15,10 @@ function getScreenshotName(basePath) {
 exports.config = {
 	suites: {
 		bidders: ['./tests/specs/bidders/*.test.js'],
+		'hivi-templates': ['./tests/specs/templates/hivi/*.test.js'],
+		'other-templates': ['./tests/specs/templates/non-hivi/*.test.js'],
 		services: ['./tests/specs/services/*.test.js'],
 		slots: ['./tests/specs/slots/*.test.js'],
-		hiviTemplates: ['./tests/specs/templates/hivi/*.test.js'],
-		otherTemplates: ['./tests/specs/templates/non-hivi/*.test.js'],
 		utils: ['./tests/specs/utils/*.test.js'],
 		video: ['./tests/specs/video/*.test.js'],
 	},
