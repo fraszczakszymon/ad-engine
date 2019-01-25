a9d983a2af17181a059333ef3772270a63558200
Katarzyna Basiukajc <kbasiukajc@wikia-inc.com>
2019-01-25 14:01:36 +0100

    ADEN-8103 | Add incontent native tests

---
+++ tests/pages/incontent-native.page.js
@@ -0,0 +1,8 @@
+class IncontentNative {
+	constructor() {
+		this.pageLink = 'templates/incontent-native/';
+		this.lineItemId = '4932898489';
+	}
+}
+
+export const incontentNative = new IncontentNative();
