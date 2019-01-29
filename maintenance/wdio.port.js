52eb92194a628c1dd402dbc9372fcb6da89d8138
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2019-01-29 09:08:34 +0100

    ADEN-8190 Limit available ports

--- maintenance/wdio.port.js
+++ maintenance/wdio.port.js
@@ -1,6 +1,18 @@
 /* eslint-disable import/no-extraneous-dependencies, no-console */
 const portfinder = require('portfinder');
 
-portfinder.getPort((err, port) => {
+const properties = {};
+
+if (process.argv[2] && process.argv[3]) {
+	const startPort = parseInt(process.argv[2], 10);
+	const offset = parseInt(process.argv[3], 10);
+
+	properties.port = startPort;
+	properties.stopPort = startPort + offset;
+}
+
+console.log(properties);
+
+portfinder.getPort(properties, (err, port) => {
 	console.log(port);
 });
