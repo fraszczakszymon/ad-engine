a517148ccf3db1b172d85840cfac37bd62144795
Jacek Bronski-Jankowski <jjankowski@wikia-inc.com>
2019-02-20 08:16:07 +0100

    ADEN-8407 Nielsen cleanup.

--- src/ad-services/nielsen/index.ts
+++ src/ad-services/nielsen/index.ts
@@ -3,7 +3,7 @@ import { context, utils } from '@wikia/ad-engine';
 import { initNielsenStaticQueue } from './static-queue-script';
 
 const logGroup = 'nielsen-dcr';
-const nlsnConfig = {};
+const nlsnConfig: {[key: string]: any} = {};
 
 /**
  * Creates Nielsen Static Queue Snippet
@@ -20,11 +20,13 @@ function createInstance(nielsenKey) {
  * Nielsen service handler
  */
 class Nielsen {
+
+	nlsnInstance: any = null;
 	/**
 	 * Class constructor
 	 */
+
 	constructor() {
-		this.nlsnInstance = null;
 
 		if (utils.queryString.get('nielsen-dcr-debug') === '1') {
 			nlsnConfig.nol_sdkDebug = 'debug';
