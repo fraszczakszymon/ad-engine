10a5d712a3a2a5a9ac249905f1c5be58daad39c1
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2019-04-05 15:33:10 +0200

    ADEN-8577 initial Likho example page

---
+++ examples/services/likho/script.ts
@@ -0,0 +1,31 @@
+import { AdEngine, context, templateService } from '@wikia/ad-engine';
+import { utils } from '@wikia/ad-products';
+import customContext from '../../context';
+import '../../styles.scss';
+
+window.guaTrackEvent = (...args) => {
+	console.log(`🛤 Custom tracker: ${args}`);
+};
+
+customContext.targeting.artid = '535';
+customContext.slots.top_leaderboard.sizes = [
+	{
+		viewportSize: [728, 0],
+		sizes: [[728, 90], [3, 3]],
+	},
+	{
+		viewportSize: [970, 0],
+		sizes: [[970, 250], [3, 3]],
+	},
+];
+
+context.extend(customContext);
+
+if (document.body.offsetWidth < 728) {
+	context.set('state.isMobile', true);
+	context.set('targeting.skin', 'fandom_mobile');
+}
+
+utils.setupNpaContext();
+
+new AdEngine().init();
