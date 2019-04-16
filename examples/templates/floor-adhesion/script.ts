e3403c3dbe40f77684186b26160ddb02b9d4e345
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2019-04-16 14:19:57 +0200

    ADEN-8659 Fix out-of-page example pages

--- examples/templates/floor-adhesion/script.ts
+++ examples/templates/floor-adhesion/script.ts
@@ -1,10 +1,12 @@
-import { AdEngine, templateService } from '@wikia/ad-engine';
+import { AdEngine, context, templateService } from '@wikia/ad-engine';
 import { FloorAdhesion } from '@wikia/ad-products';
 import customContext from '../../context';
 import '../../styles.scss';
 
 customContext.targeting.artid = '158';
 
+context.extend(customContext);
+
 templateService.register(FloorAdhesion);
 
-new AdEngine(customContext).init();
+new AdEngine().init();
