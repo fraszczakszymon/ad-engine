9bb965e617f94be36a36f6bd800f6d2d997dc95d
Katarzyna Basiukajc <bckatarzyna@gmail.com>
2019-05-06 14:37:09 +0200

    ADEN-8704 | Delay UAP control panel disappearing (#378)
    * ADEN-8704 | Delay panel disappearing
    
    * ADEN-8704 | CR fixes
    
    * ADEN-8704 | Add types to timeout variables

--- src/ad-products/templates/interface/video/toggle-ui.ts
+++ src/ad-products/templates/interface/video/toggle-ui.ts
@@ -3,33 +3,53 @@ import { utils } from '@wikia/ad-engine';
 const overlayTimeout = 5000;
 
 function add(video, container, params) {
-	let timeout = null;
+	let removeVisibilityTimeout: number;
+	let fadeOutTimeout: number;
 
 	const isMobile = utils.client.isSmartphone() || utils.client.isTablet();
 	const overlay = document.createElement('div');
+	const panel = document.getElementsByClassName('dynamic-panel')[0] as HTMLElement;
 	const setAutomaticToggle = () => {
-		timeout = setTimeout(() => {
+		removeVisibilityTimeout = window.setTimeout(() => {
 			if (video.isPlaying()) {
 				video.container.classList.remove('ui-visible');
 			}
 		}, overlayTimeout);
 	};
 
+	function fadeOut(): void {
+		fadeOutTimeout = window.setTimeout(() => {
+			overlay.classList.add('fading');
+			panel.classList.add('fading');
+		}, 3000);
+		removeVisibilityTimeout = window.setTimeout(() => {
+			video.container.classList.remove('ui-visible');
+		}, 4000);
+	}
+
+	function resetFadeOut(): void {
+		clearTimeout(removeVisibilityTimeout);
+		clearTimeout(fadeOutTimeout);
+		overlay.classList.remove('fading');
+		panel.classList.remove('fading');
+	}
+
 	overlay.classList.add('toggle-ui-overlay');
 	if (isMobile) {
 		overlay.addEventListener('click', () => {
 			video.container.classList.toggle('ui-visible');
 
-			clearTimeout(timeout);
+			clearTimeout(removeVisibilityTimeout);
 			setAutomaticToggle();
 		});
 		video.addEventListener('resume', setAutomaticToggle);
 	} else {
 		video.container.addEventListener('mouseenter', () => {
 			video.container.classList.add('ui-visible');
+			resetFadeOut();
 		});
 		video.container.addEventListener('mouseleave', () => {
-			video.container.classList.remove('ui-visible');
+			fadeOut();
 		});
 		overlay.addEventListener('click', () => {
 			top.open(params.clickThroughURL, '_blank');
