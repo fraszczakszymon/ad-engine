05163d741e03bacfac1375ba62b257f64760c6de
Damian Bielecki <bielik20@gmail.com>
2019-06-25 12:01:16 +0200

    Add separate (additional) GP window types

--- @types/window/index.d.ts
+++ @types/window/index.d.ts
@@ -19,27 +19,8 @@ interface Window {
 	// No types easily available for pbjs.
 	pbjs?: any;
 	pvNumber?: number;
+	pvNumberGlobal?: number;
+	adsQueue?: any;
 }
 
 declare var NOLBUNDLE: any;
-
-// TODO: It's from Gamepedia, need to move somewhere.
-interface Window {
-	__cmp?: any;
-	__cmpStored?: any;
-	// ads: Ads;
-	adsQueue?: any;
-	mw?: any;
-	pbjs?: any;
-	RLQ?: any;
-	XMLHttpRequest?: any;
-	sessionId?: string;
-	pvNumber?: number;
-	pvNumberGlobal?: number;
-	pvUID?: string;
-	wgWikiaCookieDomain?: string;
-	wgCookiePath?: string;
-	beacon_id?: string;
-	session_id?: string;
-	wikiaSessionId?: string;
-}
---
+++ platforms/gamepedia/@types/window/index.d.ts
@@ -0,0 +1,15 @@
+interface Window {
+	__cmp?: any;
+	__cmpStored?: any;
+	// ads: Ads;
+	mw?: any;
+	RLQ?: any;
+	XMLHttpRequest?: any;
+	sessionId?: string;
+	pvUID?: string;
+	wgWikiaCookieDomain?: string;
+	wgCookiePath?: string;
+	beacon_id?: string;
+	session_id?: string;
+	wikiaSessionId?: string;
+}
