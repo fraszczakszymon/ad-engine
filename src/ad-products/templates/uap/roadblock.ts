bc63dc531bc7b695b3e8050cb613cf364b119f91
Katarzyna Basiukajc <bckatarzyna@gmail.com>
2019-07-30 11:12:20 +0200

    ADEN-9073 | Add roadblock template to Gamepedia (#463)

--- src/ad-products/templates/uap/roadblock.ts
+++ src/ad-products/templates/uap/roadblock.ts
@@ -1,18 +1,26 @@
 import { context } from '@ad-engine/core';
 import { universalAdPackage } from './universal-ad-package';
 
+export interface RoadblockTemplateConfig {
+	slotsToEnable: string[];
+	slotsToDisable: string[];
+	onInit: () => void;
+}
+
 export class Roadblock {
-	static getName() {
+	static getName(): string {
 		return 'roadblock';
 	}
 
-	static getDefaultConfig() {
+	static getDefaultConfig(): RoadblockTemplateConfig {
 		return {
 			slotsToEnable: [],
 			slotsToDisable: [],
 			onInit: () => {},
 		};
 	}
+
+	config: RoadblockTemplateConfig;
 
 	constructor() {
 		this.config = context.get('templates.roadblock') || {};
@@ -21,7 +29,7 @@ export class Roadblock {
 	/**
 	 * Initializes the Roadblock unit
 	 */
-	init(params) {
+	init(params: {}): void {
 		this.params = params;
 		this.params.adProduct = 'ruap';
 		universalAdPackage.init(this.params, this.config.slotsToEnable, this.config.slotsToDisable);
