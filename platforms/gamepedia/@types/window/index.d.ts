8bc368a3a025f6b91cd69a4dd8198acc91bb1193
Damian Bielecki <bielik20@gmail.com>
2019-08-06 11:25:31 +0200

    clean types

--- platforms/gamepedia/@types/window/index.d.ts
+++ platforms/gamepedia/@types/window/index.d.ts
@@ -2,12 +2,8 @@ interface Window {
 	__cmpStored?: WindowCMP;
 	mw: MediaWiki;
 	RLQ?: any;
-	XMLHttpRequest?: any;
-	sessionId?: string;
 	pvUID?: string;
-	wgWikiaCookieDomain?: string;
 	wgCookiePath?: string;
 	beacon_id?: string;
 	session_id?: string;
-	wikiaSessionId?: string;
 }
--- platforms/mutfut/@types/window/index.d.ts
+++ platforms/mutfut/@types/window/index.d.ts
@@ -1,12 +1,3 @@
 interface Window {
 	__cmpStored?: WindowCMP;
-	RLQ?: any;
-	XMLHttpRequest?: any;
-	sessionId?: string;
-	pvUID?: string;
-	wgWikiaCookieDomain?: string;
-	wgCookiePath?: string;
-	beacon_id?: string;
-	session_id?: string;
-	wikiaSessionId?: string;
 }
