e4c2188ac7735761e2df60839ad74108e6facc7e
Damian Bielecki <bielik20@gmail.com>
2019-08-06 10:14:50 +0200

    fix missing context (working ads)

--- platforms/mutfut/index.ts
+++ platforms/mutfut/index.ts
@@ -1,4 +1,5 @@
 import { context } from '@wikia/ad-engine';
+import { basicContext } from '../gamepedia/ad-context';
 import { cmpWrapper } from './cmp/cmp-wrapper';
 import { setupAdEngine } from './setup-ad-engine';
 import './styles.scss';
@@ -6,6 +7,10 @@ import './styles.scss';
 const geo = 'PL';
 
 context.set('country', geo);
+context.set('custom.isCMPEnabled', cmpWrapper.geoRequiresConsent(geo));
+context.set('options.geoRequiresConsent', cmpWrapper.geoRequiresConsent(geo));
+
 cmpWrapper.init().then(() => {
+	context.extend(basicContext);
 	cmpWrapper.getConsent(geo).then((response) => setupAdEngine(response));
 });
