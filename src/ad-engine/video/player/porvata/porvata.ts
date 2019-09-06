4bf4664ee15b11119432e5305db44da03abfd834
Tomasz Felczak <43118980+preludek@users.noreply.github.com>
2019-09-06 15:04:13 +0200

    ADEN-9173-1 Fixed porvata DW tracking (#509)

--- src/ad-engine/video/player/porvata/porvata.ts
+++ src/ad-engine/video/player/porvata/porvata.ts
@@ -348,7 +348,7 @@ export class PorvataFiller implements SlotFiller {
 		this.porvataParams.slotName = adSlot.getSlotName();
 
 		templateService.init(this.porvataParams.type, adSlot, this.porvataParams);
-		adSlot.emitEvent(AdSlot.STATUS_SUCCESS);
+		adSlot.setConfigProperty('trackEachStatus', true);
 	}
 
 	getContainer(): HTMLElement {
