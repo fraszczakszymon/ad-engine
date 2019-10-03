30b9e51b3c915b389c43d8c7950892ec75696adf
Damian Bielecki <bielik20@gmail.com>
2019-10-03 11:50:50 +0200

    Revert "fix build size"
    This reverts commit cb1028663c1d9e320e7e870f9021a4611fd75037.

--- webpack.platforms.js
+++ webpack.platforms.js
@@ -3,35 +3,33 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
-const platforms = ({ platform }) => {
-	return {
-		entry: path.resolve(__dirname, `platforms/${platform}/index.ts`),
+const platforms = {
+	entry: {
+		gamepedia: path.resolve(__dirname, `platforms/gamepedia/index.ts`),
+		sports: path.resolve(__dirname, `platforms/sports/index.ts`),
+	},
 
-		output: {
-			filename: 'main.bundle.js',
-			path: path.resolve(__dirname, `dist/platforms/${platform}`),
-		},
+	output: {
+		filename: '[name]/main.bundle.js',
+		path: path.resolve(__dirname, `dist/platforms`),
+	},
 
-		plugins: [new MiniCssExtractPlugin({ filename: 'styles.css' })],
+	plugins: [new MiniCssExtractPlugin({ filename: '[name]/styles.css' })],
 
-		performance: {
-			maxAssetSize: 310000,
-			maxEntrypointSize: 330000,
-		},
+	performance: {
+		maxAssetSize: 310000,
+		maxEntrypointSize: 330000,
+	},
 
-		devServer: {
-			inline: false,
-			port: 9000,
-			contentBase: `dist/platforms`,
-		},
+	devServer: {
+		inline: false,
+		port: 9000,
+		contentBase: `dist/platforms`,
+	},
 
-		devtool: 'source-map',
-	};
+	devtool: 'source-map',
 };
 
 module.exports = () => {
-	return [
-		merge(common(), platforms({ platform: 'gamepedia' })),
-		merge(common(), platforms({ platform: 'sports' })),
-	];
+	return merge(common(), platforms);
 };
