095e9f09e76d5dedcbc51091a3aa81b2be6f54ee
Tomasz Felczak <tfelczak@wikia-inc.com>
2019-11-13 16:53:40 +0100

    ADEN-9581 added set ups - context, state, aerunner, wikicontext

--- platforms/ucp/setup-ucp-ioc.ts
+++ platforms/ucp/setup-ucp-ioc.ts
@@ -1,13 +1,29 @@
+import {
+	AdEngineRunnerSetup,
+	CommonAdEngineRunnerSetup,
+	CommonContextSetup,
+	CommonStateSetup,
+	ContextSetup,
+	StateSetup,
+	WikiContextSetup,
+} from '@platforms/shared';
 import { context, InstantConfigService } from '@wikia/ad-engine';
 import { Container } from '@wikia/dependency-injection';
 import { set } from 'lodash';
+
 import * as fallbackInstantConfig from './fallback-config.json';
+
+import { UcpWikiContextSetup } from './setup/context/wiki/ucp-wiki-context.setup';
 
 export async function setupUcpIoc(): Promise<Container> {
 	const container = new Container();
 
 	set(window, context.get('services.instantConfig.fallbackConfigKey'), fallbackInstantConfig);
 	container.bind(InstantConfigService as any).value(await InstantConfigService.init());
+	container.bind(WikiContextSetup).to(UcpWikiContextSetup);
+	container.bind(AdEngineRunnerSetup).to(CommonAdEngineRunnerSetup);
+	container.bind(ContextSetup).to(CommonContextSetup);
+	container.bind(StateSetup).to(CommonStateSetup);
 
 	return container;
 }
