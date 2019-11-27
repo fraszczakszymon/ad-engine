75a670f59ee131fb874f664ab263e42d5db00d3c
Paulina Gralak <pgralak@wikia-inc.com>
2019-11-27 13:44:02 +0100

    [ADEN-9569] OneVideo config changed

--- src/ad-bidders/prebid/adapters/one-video.ts
+++ src/ad-bidders/prebid/adapters/one-video.ts
@@ -13,12 +13,18 @@ export class OneVideo extends PrebidAdapter {
 			mediaTypes: {
 				video: {
 					playerSize: [640, 480],
+					context: 'instream',
 				},
 			},
 			bids: [
 				{
 					bidder: this.bidderName,
 					params: {
+						video: {
+							playerWidth: 480,
+							playerHeight: 640,
+							mimes: ['video/mp4'],
+						},
 						pubId,
 					},
 				},
