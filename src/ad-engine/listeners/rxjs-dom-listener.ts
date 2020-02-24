a14a2a568c13b94ce58b8c5e6127030123d128f7
Damian Bielecki <bielik20@gmail.com>
2020-02-24 12:00:24 +0100

    add resize listener

--- src/ad-engine/listeners/rxjs-dom-listener.ts
+++ src/ad-engine/listeners/rxjs-dom-listener.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@wikia/dependency-injection';
 import { animationFrameScheduler, fromEvent, Observable } from 'rxjs';
 import { observeOn, publish, refCount } from 'rxjs/operators';
 
+type FromEventTarget<T> = Parameters<typeof fromEvent>[0];
+
 @Injectable()
 export class RxjsDomListener {
-	readonly scroll$: Observable<Event> = this.createSource('scroll');
-	readonly resize$: Observable<Event> = this.createSource('resize');
+	readonly scroll$: Observable<Event> = this.createSource(document, 'scroll');
+	readonly resize$: Observable<Event> = this.createSource(window, 'resize');
 
-	private createSource(eventName: string): Observable<Event> {
-		return fromEvent(document, eventName).pipe(
+	private createSource<T>(target: FromEventTarget<T>, eventName: string): Observable<T> {
+		return fromEvent(target, eventName).pipe(
 			observeOn(animationFrameScheduler), // scheduler to ensure smooth animation
 			publish(), // so that only one listener is created
 			refCount(), // so that listener is deleted when no subscriptions
