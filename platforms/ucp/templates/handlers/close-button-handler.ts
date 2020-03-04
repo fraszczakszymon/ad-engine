2a153dff53915ee696a4a2ab38afed818b555095
Szymon Frąszczak <fraszczak.szymon+github@gmail.com>
2020-03-04 12:00:51 +0100

    Show close button once user scroll (#683)
    Co-authored-by: Emil Madry <40793618+emadry@users.noreply.github.com>

--- platforms/ucp/templates/handlers/close-button-handler.ts
+++ platforms/ucp/templates/handlers/close-button-handler.ts
@@ -1,18 +1,25 @@
 import {
 	AdSlot,
 	CloseButton,
+	RxjsDomListener,
 	TEMPLATE,
 	TemplateStateHandler,
 	TemplateTransition,
 	universalAdPackage,
 } from '@wikia/ad-engine';
 import { Inject, Injectable } from '@wikia/dependency-injection';
+import { Subject } from 'rxjs';
+import { filter, take, takeUntil, tap } from 'rxjs/operators';
 
 @Injectable()
 export class CloseButtonHandler implements TemplateStateHandler {
+	private unsubscribe$ = new Subject<void>();
 	private button: HTMLButtonElement;
 
-	constructor(@Inject(TEMPLATE.SLOT) private adSlot: AdSlot) {}
+	constructor(
+		@Inject(TEMPLATE.SLOT) private adSlot: AdSlot,
+		private domListener: RxjsDomListener,
+	) {}
 
 	async onEnter(transition: TemplateTransition<'transition'>): Promise<void> {
 		this.button = new CloseButton({
@@ -21,10 +28,23 @@ export class CloseButtonHandler implements TemplateStateHandler {
 				transition('transition');
 			},
 		}).render();
-		this.adSlot.getElement().appendChild(this.button);
+
+		this.domListener.scroll$
+			.pipe(
+				takeUntil(this.unsubscribe$),
+				filter(() => {
+					return window.scrollY > 0;
+				}),
+				take(1),
+				tap(() => {
+					this.adSlot.getElement().appendChild(this.button);
+				}),
+			)
+			.subscribe();
 	}
 
 	async onLeave(): Promise<void> {
+		this.unsubscribe$.next();
 		this.button.remove();
 	}
 }
