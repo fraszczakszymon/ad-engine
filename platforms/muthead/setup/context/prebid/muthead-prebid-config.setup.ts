8e35f9ec33edd3ecce1c89aa8e7af851eb70e4e8
Paulina Gralak <pgralak@wikia-inc.com>
2020-10-01 12:02:34 +0200

    [ADEN-10464] Gamepedia and Sports: remove OpenX integration (#902)
    * [ADEN-10464] Gamepedia: remove OpenX integration
    
    * [ADEN-10464] Futhead: remove OpenX integration
    
    * [ADEN-10464] Muthead: remove OpenX integration
    
    Co-authored-by: Damian Sobczak <35136242+dmnsobczak@users.noreply.github.com>

--- platforms/muthead/setup/context/prebid/muthead-prebid-config.setup.ts
+++ platforms/muthead/setup/context/prebid/muthead-prebid-config.setup.ts
@@ -4,7 +4,6 @@ import { Injectable } from '@wikia/dependency-injection';
 import { getAppNexusContext } from '../../../bidders/prebid/app-nexus';
 import { getIndexExchangeContext } from '../../../bidders/prebid/index-exchange';
 import { getNobidContext } from '../../../bidders/prebid/nobid';
-import { getOpenXContext } from '../../../bidders/prebid/openx';
 import { getPubmaticContext } from '../../../bidders/prebid/pubmatic';
 import { getRubiconContext } from '../../../bidders/prebid/rubicon';
 import { getTripleliftContext } from '../../../bidders/prebid/triplelift';
@@ -17,7 +16,6 @@ export class MutheadPrebidConfigSetup implements DiProcess {
 		context.set('bidders.prebid.appnexus', getAppNexusContext(mode));
 		context.set('bidders.prebid.indexExchange', getIndexExchangeContext(mode));
 		context.set('bidders.prebid.nobid', getNobidContext(mode));
-		context.set('bidders.prebid.openx', getOpenXContext(mode));
 		context.set('bidders.prebid.pubmatic', getPubmaticContext(mode));
 		context.set('bidders.prebid.rubicon_display', getRubiconContext(mode));
 		context.set('bidders.prebid.triplelift', getTripleliftContext(mode));
--- platforms/shared/context/prebid/gamepedia-prebid-context-setup.ts
+++ platforms/shared/context/prebid/gamepedia-prebid-context-setup.ts
@@ -4,7 +4,6 @@ import { Injectable } from '@wikia/dependency-injection';
 import { getGamepediaAppNexusContext } from '../../bidders/gamepedia/app-nexus';
 import { getGamepediaIndexExchangeContext } from '../../bidders/gamepedia/index-exchange';
 import { getGamepediaNobidContext } from '../../bidders/gamepedia/nobid';
-import { getGamepediaOpenXContext } from '../../bidders/gamepedia/openx';
 import { getGamepediaPubmaticContext } from '../../bidders/gamepedia/pubmatic';
 import { getGamepediaRubiconContext } from '../../bidders/gamepedia/rubicon';
 import { getGamepediaTripleliftContext } from '../../bidders/gamepedia/triplelift';
@@ -19,7 +18,6 @@ export class UcpGamepediaPrebidConfigSetup implements DiProcess {
 		context.set('bidders.prebid.appnexus', getGamepediaAppNexusContext(mode));
 		context.set('bidders.prebid.indexExchange', getGamepediaIndexExchangeContext(mode));
 		context.set('bidders.prebid.nobid', getGamepediaNobidContext(mode));
-		context.set('bidders.prebid.openx', getGamepediaOpenXContext(mode));
 		context.set('bidders.prebid.pubmatic', getGamepediaPubmaticContext(mode));
 		context.set('bidders.prebid.rubicon_display', getGamepediaRubiconContext(mode));
 		context.set('bidders.prebid.triplelift', getGamepediaTripleliftContext(mode));
