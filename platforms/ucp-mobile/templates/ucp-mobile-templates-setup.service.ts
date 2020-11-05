43fe99f79352d7909ba0339ee443dbc1df83a958
Damian Sobczak <35136242+dmnsobczak@users.noreply.github.com>
2020-11-05 11:56:26 +0100

    ADEN-10452 Adding UAP template to New Mobile Experience (#925)
    Co-authored-by: Andrzej Łukaszewski <nandy-andy@users.noreply.github.com>

--- platforms/ucp-mobile/templates/ucp-mobile-templates-setup.service.ts
+++ platforms/ucp-mobile/templates/ucp-mobile-templates-setup.service.ts
@@ -9,6 +9,8 @@ import {
 } from '@wikia/ad-engine';
 import { Injectable } from '@wikia/dependency-injection';
 import { merge } from 'rxjs';
+import { registerBfaaTemplate } from './bfaa-template';
+import { registerBfabTemplate } from './bfab-template';
 import { getOutstreamConfig } from './configs/outstream-config';
 
 @Injectable()
@@ -20,8 +22,10 @@ export class UcpMobileTemplatesSetup implements DiProcess {
 	execute(): void {
 		const floorAdhesion$ = registerFloorAdhesionTemplate(this.registry);
 		const interstitial$ = registerInterstitialTemplate(this.registry);
+		const bfaa$ = registerBfaaTemplate(this.registry);
+		const bfab$ = registerBfabTemplate(this.registry);
 
-		logTemplates(merge(floorAdhesion$, interstitial$));
+		logTemplates(merge(floorAdhesion$, interstitial$, bfaa$, bfab$));
 
 		templateService.register(AffiliateDisclaimer);
 		templateService.register(PorvataTemplate, getOutstreamConfig());
