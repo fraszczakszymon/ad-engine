23cb96ff9fcd2b85ca0e81575add197df4421440
Paulina Gralak <pgralak@wikia-inc.com>
2020-12-07 14:34:37 +0100

    ADEN-10495 | Permutive unit tests

---
+++ spec/ad-services/permutive/permutive.spec.ts
@@ -0,0 +1,94 @@
+import { expect } from 'chai';
+import { createSandbox } from 'sinon';
+import { context, utils } from '../../../src/ad-engine';
+import { permutive } from '../../../src/ad-services';
+
+describe.only('Permutive', () => {
+	const sandbox = createSandbox();
+	let loadScriptStub;
+
+	// @ts-ignore
+	window.localStorage = {
+		_psegs: '[1, 2, 3, 1000000, 1000001, 999999, 0, -7]',
+		_ppam: '["123", "456"]',
+
+		getItem(key: string): string | null {
+			return this[key];
+		},
+	};
+
+	beforeEach(() => {
+		loadScriptStub = sandbox
+			.stub(utils.scriptLoader, 'loadScript')
+			.returns(Promise.resolve({} as any));
+		context.set('services.permutive.enabled', true);
+		context.set('options.trackingOptIn', true);
+		context.set('options.optOutSale', false);
+		context.set('wiki.targeting.directedAtChildren', false);
+	});
+
+	afterEach(() => {
+		sandbox.restore();
+	});
+
+	it('Permutive is called', async () => {
+		await permutive.call();
+
+		expect(loadScriptStub.called).to.equal(true);
+	});
+
+	it('Permutive can be disabled', async () => {
+		context.set('services.Permutive.enabled', false);
+
+		await permutive.call();
+
+		expect(loadScriptStub.called).to.equal(false);
+	});
+
+	it('Permutive not called when user is not opted in', async () => {
+		context.set('options.trackingOptIn', false);
+
+		await permutive.call();
+
+		expect(loadScriptStub.called).to.equal(false);
+	});
+
+	it('Permutive not called when user has opted out sale', async () => {
+		context.set('options.optOutSale', true);
+
+		await permutive.call();
+
+		expect(loadScriptStub.called).to.equal(false);
+	});
+
+	it('Permutive not called on kid wikis', async () => {
+		context.set('wiki.targeting.directedAtChildren', true);
+
+		await permutive.call();
+
+		expect(loadScriptStub.called).to.equal(false);
+	});
+
+	it('Permutive keys for bidders set in context', async () => {
+		await permutive.call();
+
+		const permutiveKeys = context.get('bidders.permutiveKeys');
+
+		expect(permutiveKeys).to.eql(['1000000', '1000001', '123', '456']);
+	});
+
+	it('No Permutive keys for bidders set in context', async () => {
+		// @ts-ignore
+		window.localStorage = {
+			getItem(key: string): string | null {
+				return this[key];
+			},
+		};
+
+		await permutive.call();
+
+		const permutiveKeys = context.get('bidders.permutiveKeys');
+
+		expect(permutiveKeys).to.eql([]);
+	});
+});
